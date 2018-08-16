e9e22e4c632427a5c05521592f39d76cb48f398c
Christian <cdefaria@mail.csuchico.edu>
2018-08-16 16:29:10 -0400

    Added models and adjusted schema.sql

--- schema.sql
+++ schema.sql
@@ -10,19 +10,6 @@ date_created TIMESTAMP,
 number_of_weeks INTEGER,
 PRIMARY KEY(id));
 
-/*
-create curriculum_topics linking table
-*/
-CREATE SEQUENCE curriculum_topic_seq
-INCREMENT BY 1;
-
-CREATE TABLE curriculum_topic
-(id INTEGER NOT NULL default curriculum_topic_seq,
-curriculum_id INTEGER,
-topic_id INTEGER,
-week_number INTEGER,
-PRIMARY KEY (id));
-
 CREATE SEQUENCE topic_seq
 INCREMENT BY 1;
 
@@ -41,3 +28,16 @@ name VARCHAR(25),
 parent_topic_id INTEGER,
 PRIMARY KEY(id),
 FOREIGN KEY (parent_topic_id) references topic(id));
+
+/*
+create curriculum_topics linking table
+*/
+CREATE SEQUENCE curriculum_topics_seq
+INCREMENT BY 1;
+
+CREATE TABLE curriculum_topics
+(id INTEGER NOT NULL default curriculum_topics_seq,
+curriculum_id INTEGER,
+topic_id INTEGER,
+week_number INTEGER,
+PRIMARY KEY (id));
