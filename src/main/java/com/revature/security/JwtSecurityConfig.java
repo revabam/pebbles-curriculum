cde6bc935548c1f7b9a01a02f0d923c52264b8bd
AlexMoraga <alex.moraga94@gmail.com>
2018-10-01 22:40:29 -0400

    yml files updated

--- src/main/java/com/revature/security/JwtSecurityConfig.java
+++ src/main/java/com/revature/security/JwtSecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  *
  */
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Configuration
 public class JwtSecurityConfig extends WebSecurityConfigurerAdapter implements Ordered {
 
