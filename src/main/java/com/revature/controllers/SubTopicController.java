598c6a8b702ab4cea5d83dd424ca9e5d5d7ca4ec
Rhys_Yamasaki <yamasaki.rhys@gmail.com>
2018-10-02 13:37:15 -0400

    renamed methods to match methods implementation

--- src/main/java/com/revature/controllers/SubTopicController.java
+++ src/main/java/com/revature/controllers/SubTopicController.java
@@ -43,12 +43,12 @@ public class SubTopicController {
 	 * @author Christian DeFaria 1806-Jun18-USF-Java Wezley Singleton
 	 */
 	@PostMapping("/day")
-	public ResponseEntity<DaySubTopic> addSubtopic(@RequestBody DaySubTopic newSubtopic) {
+	public ResponseEntity<DaySubTopic> addDaySubtopic(@RequestBody DaySubTopic newSubtopic) {
 		System.out.println("CREATING SUBTOPIC " + newSubtopic);
 		DaySubTopic subtopic = service.addSubtopic(newSubtopic);
 		return new ResponseEntity<>(subtopic, HttpStatus.CREATED);
 	}
-
+	
 	/**
 	 * Updates a subtopic in the database
 	 *
@@ -57,7 +57,7 @@ public class SubTopicController {
 	 * @author Christian DeFaria 1806-Jun18-USF-Java Wezley Singleton
 	 */
 	@PutMapping("/day")
-	public ResponseEntity<DaySubTopic> updateSubtopic(@Valid @RequestBody DaySubTopic updatedSubtopic) {
+	public ResponseEntity<DaySubTopic> updateDaySubtopic(@Valid @RequestBody DaySubTopic updatedSubtopic) {
 		DaySubTopic subtopic = service.updateSubtopic(updatedSubtopic);
 		return new ResponseEntity<>(subtopic, HttpStatus.OK);
 	}
@@ -82,7 +82,7 @@ public class SubTopicController {
 	 * @author Beck Larson | Spark 1806 June25 2018 | USF | Steven Kelsey
 	 */
 	@DeleteMapping("/day/{id}")
-	public ResponseEntity<HttpStatus> deleteSubTopic(@PathVariable int id){
+	public ResponseEntity<HttpStatus> deleteDaySubTopic(@PathVariable int id){
 		service.deleteSubtopic(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -94,7 +94,7 @@ public class SubTopicController {
 	 * @author Chris Holmes | Spark 1806 June25 2018 | USF | Steven Kelsey
 	 */
 	@DeleteMapping("/{id}")
-	public ResponseEntity<HttpStatus> deleteDaySubTopic(@PathVariable int id){
+	public ResponseEntity<HttpStatus> deleteSubTopic(@PathVariable int id){
 		service.delete(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -125,6 +125,13 @@ public class SubTopicController {
 	public ResponseEntity<SubtopicNames> getBySubtopicId(@PathVariable int id) {
 		SubtopicNames value = service.getBySubtopicId(id);
 		return new ResponseEntity<>(value, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<DaySubTopic> addSubtopic(@RequestBody DaySubTopic newSubtopic) {
+		System.out.println("CREATING SUBTOPIC " + newSubtopic);
+		DaySubTopic subtopic = service.addSubtopic(newSubtopic);
+		return new ResponseEntity<>(subtopic, HttpStatus.CREATED);
 	}
 
 	/**
